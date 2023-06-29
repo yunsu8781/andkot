03cde8953c871f98ab2c3c1ba82e260ddde4cd85
yunsu <lws8905@gmail.com>
2023-06-29 14:36:32 +0900

    androidKot 8st mbti commit

--- myword/src/main/java/com/example/myword/MainActivity.kt
+++ myword/src/main/java/com/example/myword/MainActivity.kt
@@ -15,15 +15,23 @@ class MainActivity : ComponentActivity() {
 
     private fun getWord(): String {
         val list = listOf(
-            "나까지 나설 필요는 없다",
-            "헌신하면 헌신짝 된다",
-            "참고 참고 또 참으면 참나무가 된다",
-            "포기하면 편하다",
-            "왕관을 쓰려는 자, 그 무게를 견뎌라",
-            "아니면 말고",
-            "나도 나지만 너도 너다",
-            "목숨을 버리면 무기만은 살려 주겠다",
-            "가는 말이 고우면 사람을 얕본다",
+            "istj",
+            "istp",
+            "infj",
+            "intj",
+            "isfj",
+            "isfp",
+            "infp",
+            "intp",
+            "estj",
+            "esfp",
+            "enfp",
+            "entp",
+            "esfj",
+            "esfj",
+            "estp",
+            "enfj",
+            "entj"
         )
         return list.random()
     }
